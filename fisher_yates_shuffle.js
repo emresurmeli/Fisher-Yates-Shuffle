d7a741151a5212f18241d43be436c970abc8852c
Emre Surmeli <s.emre.s.8@gmail.com>
2015-01-06 14:46:47 -0800

    the shuffle

---
+++ fisher_yates_shuffle.js
@@ -0,0 +1,14 @@
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var shuffle = function (array) {
+  var m = array.length, t, i;
+
+  while(m) {
+    i = Math.floor(Math.random() * m--);
+
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
